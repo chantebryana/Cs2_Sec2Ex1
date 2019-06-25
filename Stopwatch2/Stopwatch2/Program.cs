cb9c5ae27d9b471bfeaa14508fd52957de03b5dd
Chante Earthwell <chante.earthwell@buildertrend.com>
2019-06-25 16:37:03 -0500

    stopwatch start trying to get smarter w/ user input. kind of a mess right now. work in progress.

--- Stopwatch2/Stopwatch2/Program.cs
+++ Stopwatch2/Stopwatch2/Program.cs
@@ -7,8 +7,16 @@ namespace Stopwatch2
         static void Main(string[] args)
         {
             var stopwatch = new Stopwatch();
-            stopwatch.Start = DateTime.Now;
-            Console.WriteLine(stopwatch.Start);
+            //stopwatch.Start = DateTime.Now;
+            //Console.WriteLine(stopwatch.Start);
+
+            Console.WriteLine("Type 'Start' to start stopwatch: ");
+            if (Console.ReadLine() == "Start")
+            {
+                stopwatch.Start = DateTime.Now;
+            }
+            //stopwatch.Start = DateTime.Now;
+            Console.WriteLine("Stopwatch started at '{0}'", stopwatch.Start);
         }
     }
 }
